00b7cc2f265c41412d13455a9fe9d82feece69fc
Hugo Cardoso <hugorafaelmc@gmail.com>
2023-01-29 07:54:19 +0000

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/SignUp.spec.ts
+++ src/presentation/controllers/signup/SignUp.spec.ts
@@ -214,4 +214,25 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Deve retornar 200 se os valores fornecidos forem validos', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'Hugo',
+        email: 'email@teste.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    })
+  })
 })
